eab71b5fbbfa3b0aec738e46cd789bbf4b3bc2c5
Mihai Adrian Andrei <mihai.andrei1597@gmail.com>
2024-08-09 17:29:09 +0300

    added hono + lucia auth

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+import { redirect, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { json, useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,42 +8,25 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export function loader({ context }: LoaderFunctionArgs) {
+  const { user } = context;
+  if (!user) {
+		return redirect("/login");
+	}
+  return json({
+    user,
+  });
+}
 export default function Index() {
+  const {user} = useLoaderData<typeof loader>();
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+		<>
+			<h1>Hi, {user.username}!</h1>
+			<p>Your user ID is {user.id}.</p>
+			<form action="/api/logout" method="post">
+				<button>Sign out</button>
+			</form>
+		</>
+	);
 }
